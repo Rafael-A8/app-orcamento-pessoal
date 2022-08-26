362c535afc16d0accb6890b289defb41caa5de8c
Rafael-A8 <83028364+Rafael-A8@users.noreply.github.com>
2022-08-26 17:04:28 -0300

    mudando o model para ficar mesmo verboso

--- app.js/principal.js
+++ app.js/principal.js
@@ -69,12 +69,26 @@ function cadastrarDespesa() {
     )
     
     if(despesa.validarDados()) {
-        //dialog de sucesso
+        
         bd.gravar(despesa)
-        $('#sucessoGravacao').modal('show')
+        document.getElementById('modal_titulo').innerHTML = 'Registro Inserido com Sucesso!!'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+        document.getElementById('modal_conteudo').innerHTML = 'Despesa foi Cadastrada com Sucesso!'
+        document.getElementById('modal_btn').innerHTML ='Voltar'
+        document.getElementById('modal_btn').className ='btn btn-success'
+        
+        //dialog de sucesso
+        $('#modalRegistraDespesa').modal('show')
     } else {
+         
+        document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do Registro'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+        document.getElementById('modal_conteudo').innerHTML = 'Erro na Gravação, verifique se todos os campos foram preenchidos corretamente.'
+        document.getElementById('modal_btn').innerHTML ='Voltar e corrigir'
+        document.getElementById('modal_btn').className ='btn btn-danger'
+
         //dialog de erro
-        $('#erroGravacao').modal('show')
+        $('#modalRegistraDespesa').modal('show')
     }
     
 }
