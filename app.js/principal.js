e82650a2644800a9ec6ce93660524f83c765e9ec
Alvarino <83028364+Rafael-A8@users.noreply.github.com>
2022-08-27 17:40:17 -0300

    update

--- app.js/principal.js
+++ app.js/principal.js
@@ -64,6 +64,9 @@ class Bd {
 
         return despesas
     }
+    pesquisar(despesa) {
+        console.log(despesa)
+    }
 }
 
 let bd = new Bd()
@@ -101,6 +104,13 @@ function cadastrarDespesa() {
         
         //dialog de sucesso
         $('#modalRegistraDespesa').modal('show')
+
+        ano.value = ''
+        mes.value = ''
+        dia.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
     } else {
          
         document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do Registro'
@@ -162,3 +172,15 @@ function carregaListasDespesas() {
     
 }
 
+function pesquisarDespesa () {
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+    bd.pesquisar(despesa)
+} 
